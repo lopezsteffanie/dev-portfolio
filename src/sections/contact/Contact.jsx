c22fecb8ec8b2fceaaebd8ded34c29391c1a318e
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-06-14 10:38:55 -0400

    Add contact section

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import contacts from './data'
 import './Contact.css'
 
 const Contact = () => {
     return (
-        <section id="contact"><h2>Contacts</h2></section>
+        <section id="contact">
+            <h2>Get In Touch</h2>
+            <p>
+                Shoot me a message via the link below!
+            </p>
+            <div className="container contact__container">
+                {
+                    contacts.map(contact => <a key={contact.id} href={contact.link} target='_blank' rel='noopener noreferrer'>{contact.icon}</a>)
+                }
+            </div>
+        </section>
     )
 }
 
---
+++ src/sections/contact/data.js
@@ -0,0 +1,8 @@
+import {HiOutlineMail} from 'react-icons/hi'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:steffanie@stevie-codes-it.com'}
+]
+
+
+export default data
