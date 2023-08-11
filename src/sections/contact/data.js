0d2801ef2fad013d0d8e8b41b8fb60672f75022f
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-08-10 22:02:39 -0400

    Updating details

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -1,7 +1,7 @@
 import {HiOutlineMail} from 'react-icons/hi'
 
 const data = [
-    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:steffanie@stevie-codes-it.com'}
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:lopezsteffanie8@gmail.com'}
 ]
 
 
--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
                 </div>
                 <h3 data-aos='fade-up'>Stevie Lopez</h3>
                 <p data-aos='fade-up'>
-                With a strong background as a teacher specializing in mathematics, I transitioned into software engineering with a focus on back-end development. Leveraging my experience in education, I bring a unique perspective to my team, enabling me to tackle problems and design solutions that optimize learning experiences, streamline processes, and enhance accessibility to all users.
+                With a strong background as a teacher specializing in mathematics, I transitioned into software engineering with a focus on back-end development. Leveraging my experience in education, I bring a unique perspective to my team, enabling me to tackle problems and design solutions that optimize user experiences, streamline processes, and enhance accessibility to all users.
                 </p>
                 <div className="header__cta" data-aos='fade-up'>
                     <a href="#contact" className='btn primary'>Let's Talk</a>
